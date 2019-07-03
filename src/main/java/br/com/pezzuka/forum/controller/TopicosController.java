d94d94cfe85189dc9154919c8ab1b438a43e96d6
murillo <murillo@192.168.49.18>
2019-07-03 14:38:28 -0300

    ResponseEntity.created (201); UriComponentsBuilder + URI;

--- src/main/java/br/com/pezzuka/forum/controller/TopicosController.java
+++ src/main/java/br/com/pezzuka/forum/controller/TopicosController.java
@@ -1,13 +1,16 @@
 package br.com.pezzuka.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.pezzuka.forum.controller.form.TopicoForm;
 import br.com.pezzuka.forum.dto.TopicoDTO;
@@ -38,10 +41,20 @@ public class TopicosController {
 	}
 	
 	
+	//ResponseEntity -> Usamos para retornamos os códigos de sucesso, criado etc.. 200, 201 etc...
+	//ResponseEntity.created -> É quando retornamos código 201... Porém 201 devemos retornar duas coisas
+	//1º CABEÇALHO (location, com a URI desse novo recurso)
+	//2º CORPO devolvo uma representação desse recurso criado
+	//Passamos a URI, que nós construimos através da injeção do Spring + a chamada para um método de busca por ID
+	//created -> Nós chamamos a URI que está os endereços de consulta + o recurso que acabmos de criar no Body, porém usando o DTO
 	@PostMapping
-	public void cadastrar(@RequestBody TopicoForm form) {
+	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(new TopicoDTO(topico));
 	}
 	
 	
