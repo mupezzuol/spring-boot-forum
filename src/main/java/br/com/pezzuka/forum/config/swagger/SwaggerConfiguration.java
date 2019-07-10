e66a39b75ae0874d5d81b6b494982b8ba1234edd
Murillo Pezzuol <murillopezzuol@hotmail.com>
2019-07-10 18:50:48 -0300

    Criando Parametro Global para Swagger usando Token;

--- src/main/java/br/com/pezzuka/forum/config/swagger/SwaggerConfiguration.java
+++ src/main/java/br/com/pezzuka/forum/config/swagger/SwaggerConfiguration.java
@@ -1,11 +1,15 @@
 package br.com.pezzuka.forum.config.swagger;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.pezzuka.forum.model.Usuario;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -19,8 +23,18 @@ public class SwaggerConfiguration {
 				.apis(RequestHandlerSelectors.basePackage("br.com.pezzuka.forum"))//Pacote base
 				.paths(PathSelectors.ant("/**"))//Endereços que será analisado
 				.build()
-				.ignoredParameterTypes(Usuario.class);//Ignora os end-points que retorna a classe Usuário, pois não queremos que aparece a senha etc...
-		
+				.ignoredParameterTypes(Usuario.class)//Ignora os end-points que retorna a classe Usuário, pois não queremos que aparece a senha etc...
+				
+				//Configurando parametro global, que eu quero que apareca em todos os end-points do swagger
+				//Nesse caso adc um 'Authorization'
+				.globalOperationParameters(Arrays.asList(
+						new ParameterBuilder()
+						.name("Authorization")
+						.description("Header para token JWT")//Descrição
+						.modelRef(new ModelRef("string"))//Tipo do parametro
+						.parameterType("header")//Tipo de parametro, vai no Header, não no corpo por exemplo
+						.required(false)//Não é obrigatório
+						.build()));
 	}
 	
 	
